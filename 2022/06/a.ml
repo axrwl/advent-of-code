968686baad88e55695d5406aa5718ee19634f577
Tarun Agarwal <tarun.agarwal2004@gmail.com>
2022-12-06 20:30:15 +0530

    Day 6

---
+++ 2022/06/a.ml
@@ -0,0 +1,13 @@
+open String
+module C = Set.Make(Char)
+
+let to_list s = List.init (length s) @@ get s
+
+let input = open_in "aoc.in" |> input_line |> to_list
+
+let rec aoc n = 
+  if input |> List.filteri (fun i _ -> i < n+1 && i > n-4)
+           |> C.of_list |> C.elements 
+           |> List.length = 4 then print_int @@ n+1
+  else aoc @@ n+1
+;; aoc 3
